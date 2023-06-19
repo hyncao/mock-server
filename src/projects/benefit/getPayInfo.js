c07913b70d505c106b0ab6c9b057fd619b829a11
菠菜 <277148066@qq.com>
2023-06-19 16:53:40 +0800

    update

--- src/projects/benefit/getPayInfo.js
+++ src/projects/benefit/getPayInfo.js
@@ -1,16 +1,17 @@
 module.exports = (req) => ({
-  code: 200,
+  code: 201,
   data: {
     dataList: [],
     dataMap: {
+      linkeUrl: '',
       msg: '请支付',
       openMode: 'nopay',
       payRequestInfo:
-        '{"totalAmount":100,"dealId":"1812344665","tpOrderId":"202104150200000209","dealTitle":"联通-广东存费送费(小瀑)百度测试","appKey":"MMUR4V","rsaSign":"Cks4A7rNH1xqkAbJo/f8MF9zzj4lcSK0Ytv0UuARIj4zR108mGvp+wRdYwrHmHRGgch0U6AVjsqeC94ko/9/ofHbEUEXgMPvmwH7trfU0JBq3VBHqtyH5RnnWrQLul0ob+2rxesOB/kaGt+8ai8TY54J63Vq48bVZd0Fv06fSqk=","signFieldsRange":"1"}',
-      // 2 3: '订单不存在',
-      // 4': '不符合办理条件',
+        '<form name="punchout_form" method="post" action="https://openapi.alipay.com/gateway.do?charset=UTF-8&method=alipay.trade.wap.pay&sign=aQ1ERW%2FoypFMwy4RtKbuknj%2BqUVodO2KtLYnAqxDbpunCZgCRUes3xyuavpkrNcllDPsFYaRfrm05dFRL47UE3kUaxv7yNxRBeExmgl0K4JMleInXFu%2FVqFfvq20qJbrt5cI6bPcVyWjYbaxcJtr%2BXh%2FRUkYlmQZ6mOcYvXaQ2uUANaFhJlHQoF8CvMfcrhEfD9vVARzJNGudUt6oxKVZDpIU%2Fi%2FrRq6ZUapB8L43%2Fe7IWwRI3wGWTOyXy5keGcWKK1VRN0lWxkNtPgAv05WkQDMuTVr33KrbZx9%2FQgIpitGQPhc664cA3DR9CEXl3Azj7Jp3VQH7xFvSBzS6qdT2Q%3D%3D&return_url=https%3A%2F%2Fbenefit.jujienet.com%2Fbenefit-app%2FtradeDetail%3FphoneNumber%3D13732285325%26payAmount%3D20.00%26servicePhoneNumber%3D4006660993%26tradeId%3D202306082011544736%26provinceOperatorName%3D%E6%B5%99%E6%B1%9F%E7%A7%BB%E5%8A%A8&notify_url=https%3A%2F%2Fbenefit.jujienet.com%2Fbenefit-async%2Fcallback%2FpayCallback%2F10005&version=1.0&app_id=2021001135687933&sign_type=RSA2&timestamp=2023-06-08+18%3A09%3A24&alipay_sdk=alipay-sdk-java-dynamicVersionNo&format=json&prod_code=QUICK_WAP_WAY">\n<input type="hidden" name="app_auth_token" value="202208BB1e9abf0efee54ffdaf475ddbd76a1F47">\n<input type="hidden" name="biz_content" value="{&quot;out_trade_no&quot;:202306082011544736,&quot;total_amount&quot;:20.00,&quot;quit_url&quot;:&quot;https://benefit.jujienet.com/benefit-app/tradeList&quot;,&quot;subject&quot;:&quot;全国移动流量包&quot;,&quot;timeout_express&quot;:&quot;30m&quot;,&quot;seller_id&quot;:&quot;2088701248729475&quot;}">\n<input type="submit" value="立即支付" style="display:none" >\n</form>\n<script>document.forms[0].submit();</script>',
       status: 1,
+      urlParam: '',
     },
   },
   msg: '成功',
+  success: true,
 });
